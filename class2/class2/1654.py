b1cb78206435293aef717883913a7949b25c764a
kvmii <wjdaks915@khu.ac.kr>
2024-02-07 17:43:49 +0900

    Update 1654.py

--- class2/class2/1654.py
+++ class2/class2/1654.py
@@ -1,1 +1,19 @@
+N, M = map(int,input().split())
+lan = []
+for _ in range(N):
+    lan.append(int(input()))
 
+start = 1
+end = max(lan)
+
+while start <= end:
+    mid = (start + end) // 2
+    sum  = 0
+    for num in lan:
+        sum += num // mid
+    if sum >= M:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
