c7eb0353deab89591059b1a53109f81c573f5436
kvmii <wjdaks915@khu.ac.kr>
2024-02-07 18:45:44 +0900

    Create 1874.py

---
+++ class2/class2/1874.py
@@ -0,0 +1,30 @@
+N = int(input())
+stk = []
+result = []
+
+num = 1
+fail = False
+
+for _ in range(N):
+    cnt = int(input())
+    if not stk:
+        stk.append(num)
+        num += 1
+        result.append('+')
+    if stk[-1] > cnt:
+        fail = True
+        break
+    if stk[-1] < cnt:
+        while(stk[-1] < cnt):
+            stk.append(num)
+            result.append('+')
+            num += 1
+    if stk[-1] == cnt:
+        result.append('-')
+        stk.pop()
+
+
+if fail:
+    print("NO")
+else:
+    print(word in result)
