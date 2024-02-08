124ce257b2d394d97653f974102fa1a9593b1610
Jung Min Kim <wjdaks915@khu.ac.kr>
2024-02-08 16:02:48 +0900

    Create 1966.py

---
+++ class2/class2/1966.py
@@ -0,0 +1,23 @@
+from collections import deque
+N = int(input())
+sequence = []
+for _ in range(N):
+    M, K = map(int, input().split())
+    p = list(map(int, input().split()))
+    queue = deque()
+    for i in range(len(p)):
+        queue.append((i, p[i]))
+    result = 1
+    
+    while(True):
+        big = max(queue, key=lambda x: x[1])[1]
+        num = queue.popleft()
+        if num[1] < big:
+            queue.append(num)
+        elif num[1] == big:
+            if num[0] == K:
+                sequence.append(result)
+                break
+            result += 1
+print(sequence)
+        
