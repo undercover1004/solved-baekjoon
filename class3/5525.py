046d076b70fdcc82047da08f5c0366eb05916020
Jung Min Kim <wjdaks915@khu.ac.kr>
2024-02-10 20:44:16 +0900

    Create 5525.py

---
+++ class3/5525.py
@@ -0,0 +1,21 @@
+N = int(input())
+M = int(input())
+S = input()
+
+count = 0
+result = 0
+i = 0
+while(i < M):
+    if S[i:i+3] == 'IOI':
+        print("*", i)
+        count += 1
+        i += 2
+        print(i)
+        if count == N:
+            result += 1
+            count -= 1
+    else:
+        i += 1
+        count = 0
+
+print(result)
