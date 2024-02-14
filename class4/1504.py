90bdca6b92faad59eb47ed54f5b6bce61d52782a
Jung Min Kim <wjdaks915@khu.ac.kr>
2024-02-14 19:54:43 +0900

    Create 1504.py

---
+++ class4/1504.py
@@ -0,0 +1,41 @@
+import sys
+import heapq
+N, E = map(int, input().split())
+
+
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(E):
+    a, b, c = map(int, sys.stdin.readline().split())
+    graph[a].append((b,c))
+    graph[b].append((a, c))
+
+
+def dijkstra(start, end):
+    pq = []
+    distance = [[int(1e9)] * (N + 1) for _ in range(N + 1)]
+    heapq.heappush(pq, (0, start))
+    distance[start][start] = 0
+    while pq:
+        cnt, num = heapq.heappop(pq)
+        if distance[start][num] < cnt:
+            continue
+        for x, y in graph[num]:
+            cost = cnt + y
+            if cost < distance[start][x]:
+                distance[start][x] = cost
+                heapq.heappush(pq, (cost, x))
+    return distance[start][end]
+
+
+a, b = map(int, input().split())
+res = 0
+
+res = dijkstra(1, a) + dijkstra(a, b) + dijkstra(b, N)
+res = min(res, dijkstra(1, b) + dijkstra(b, a) + dijkstra(a, N))
+
+
+if res >= int(1e9):
+    print(-1)
+else:
+    print(res)
