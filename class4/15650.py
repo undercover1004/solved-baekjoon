4d3277b6e82213e976a2b68eeb46127322a28184
Jung Min Kim <wjdaks915@khu.ac.kr>
2024-02-18 16:24:51 +0900

    Create 15650.py

---
+++ class4/15650.py
@@ -0,0 +1,19 @@
+N, M = map(int, input().split())
+res = []
+
+
+
+def bt(start):
+    if len(res) == M:
+        print(*res)
+        return
+    for i in range(start, N + 1):
+        
+        res.append(i)
+        bt(i + 1)
+        res.pop()
+
+bt(1)
+    
+
+
